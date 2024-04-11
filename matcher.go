920b88d0598fb063f42bd029a70bdaf350700448
Maxim Babichev <maksim.babichev95@gmail.com>
2024-04-12 00:37:55 +0300

    fix

--- matcher.go
+++ matcher.go
@@ -8,15 +8,23 @@ func match(query Query, stub *Stub) bool {
 	return equals(stub.Input.Equals, query.Data, stub.Input.IgnoreArrayOrder) &&
 		contains(stub.Input.Contains, query.Data, stub.Input.IgnoreArrayOrder) &&
 		matches(stub.Input.Matches, query.Data, stub.Input.IgnoreArrayOrder) &&
-		equals(stub.Headers.Equals, query.Data, false) &&
-		contains(stub.Headers.Contains, query.Data, false) &&
-		matches(stub.Headers.Matches, query.Data, false)
+		equals(stub.Headers.Equals, query.Headers, false) &&
+		contains(stub.Headers.Contains, query.Headers, false) &&
+		matches(stub.Headers.Matches, query.Headers, false)
 }
 
 func rankMatch(query Query, stub *Stub) float64 {
-	return deeply.RankMatch(stub.Input.Equals, query.Data) +
+	rank := deeply.RankMatch(stub.Input.Equals, query.Data) +
 		deeply.RankMatch(stub.Input.Contains, query.Data) +
 		deeply.RankMatch(stub.Input.Matches, query.Data)
+
+	if stub.Headers.Len() > 0 {
+		rank += deeply.RankMatch(stub.Headers.Equals, query.Headers) +
+			deeply.RankMatch(stub.Headers.Contains, query.Headers) +
+			deeply.RankMatch(stub.Headers.Matches, query.Headers)
+	}
+
+	return rank
 }
 
 func equals(expected map[string]any, actual any, orderIgnore bool) bool {
